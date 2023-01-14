86cc98837c01048c64721f85c40d38dfcf15d312
sajid-munawar <sajidmunawar666@gmail.com>
2023-01-14 16:10:38 +0500

    Resolve error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { createRoot } from 'react-dom/client';
-import TodoContainer from "./functionBased/components/TodoContainer"
+import TodoContainer from "./classBased/components/TodoContainer"
 
 //stylesheet
 import "./functionBased/App.css"
